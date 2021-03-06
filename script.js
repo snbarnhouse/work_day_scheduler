8279e0f0d81fc6daf6e9f98d01977b3032ecaea5
Stephanie Barnhouse <snbarnhouse@yahoo.com>
2021-03-05 20:13:29 -0500

    Added to Javascript and CSS files.

--- script.js
+++ script.js
@@ -1,3 +1,50 @@
 //display current date and time
 var currentDate = moment().format("MMMM Do, YYYY");
 $("#currentDay").html(currentDate);
+
+$(document).ready(function () {
+    //save button
+    $(".saveBtn").on("click", function () {
+        var text = $(this).siblings("description"). val();
+        var time = $(this).parent().attr("id");
+        //save to local storage
+        localStorage.setItem(time,text);
+    } )
+
+        function timeTrack() {
+            var presentTime = moment().hour();
+            //loop
+            $(".time-block").each(function () {
+                var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
+            //to add appropriate css styles to past, present, and future times
+                if (timeBlock === presentTime) {
+                    $(this).removeClass("past");
+                    $(this).removeClass("future");
+                    $(this).addClass("present");
+                }
+                else if (timeBlock < presentTime) {
+                    $(this).removeClass("future");
+                    $(this).removeClass("present");
+                    $(this).addClass("past");
+                }
+                else {
+                    $(this).removeClass("past");
+                    $(this).removeClass("present");
+                    $(this).addClass("future");
+                }
+            })
+        }
+        //local storage
+        $("#9am .description").val(localStorage.getItem("9am"));
+        $("#10am .description").val(localStorage.getItem("10am"));
+        $("#11am .description").val(localStorage.getItem("11am"));
+        $("#12pm .description").val(localStorage.getItem("12pm"));
+        $("#13pm .description").val(localStorage.getItem("13pm"));
+        $("#14pm .description").val(localStorage.getItem("14pm"));
+        $("#15pm .description").val(localStorage.getItem("15pm"));
+        $("#16pm .description").val(localStorage.getItem("16pm"));
+        $("#17pm .description").val(localStorage.getItem("17pm"));
+        $("#18pm .description").val(localStorage.getItem("18pm"));
+
+        timeTrack();
+})
