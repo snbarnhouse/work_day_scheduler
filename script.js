a1fb8ecb06dc00e9575b1d70fd3ba3aed3eb1269
Stephanie Barnhouse <snbarnhouse@yahoo.com>
2021-03-06 16:01:38 -0500

    Edited JS and HTML files.

--- script.js
+++ script.js
@@ -5,17 +5,24 @@ $("#currentDay").html(currentDate);
 $(document).ready(function () {
     //save button
     $(".saveBtn").on("click", function () {
-        var text = $(this).siblings("description"). val();
+        //
+        var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
         //save to local storage
+        // console.log("TEXT AND TIME: ",text,time);
+        //
         localStorage.setItem(time,text);
-    } )
+    })
 
         function timeTrack() {
             var presentTime = moment().hour();
+            console.log(presentTime) // getting an actual number
             //loop
             $(".time-block").each(function () {
-                var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
+                // var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
+                
+                var timeBlock = parseInt($(this).attr("id").slice(0,-2));
+                console.log(timeBlock) // 
             //to add appropriate css styles to past, present, and future times
                 if (timeBlock === presentTime) {
                     $(this).removeClass("past");
@@ -45,6 +52,5 @@ $(document).ready(function () {
         $("#16pm .description").val(localStorage.getItem("16pm"));
         $("#17pm .description").val(localStorage.getItem("17pm"));
         $("#18pm .description").val(localStorage.getItem("18pm"));
-
         timeTrack();
 })
